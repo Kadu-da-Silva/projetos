f10e509ede810dc8d79c66aedf8f31ce855a3430
Carlos Eduardo <carlao0010@hotmail.com>
2023-06-27 19:28:50 -0300

    Update Home.tsx

--- project-yugioh/src/pages/Home.tsx
+++ project-yugioh/src/pages/Home.tsx
@@ -3,9 +3,10 @@ import Header from "../components/Header";
 import YugiohContext from "../context/YugiohContext";
 
 export default function Home() {
-  const { loading } = useContext(YugiohContext)
+  const { loading, error } = useContext(YugiohContext)
 
   if (loading) return <div>Loading...</div>
+  if (error) return <div>Sorry, this page is not access</div>
 
   return (
     <>
